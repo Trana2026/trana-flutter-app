8218594a6222b65089432b46c8c39901eb90d84c
권태윤 <tlaw0815@gmail.com>
2026-05-14 14:58:22 +0900

    revert: 메인 페이지 원상 복구

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-// import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:trana/core/router/app_router.dart';
 import 'package:trana/core/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: '.env');
 
-  try {
-    await dotenv.load(fileName: '.env');
-  } catch (e) {
-    print("Log: .env load skip (file might be empty or missing)");
-  }
-
-  // await Supabase.initialize(
-  //   url: dotenv.env['SUPABASE_URL']!,
-  //   anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
-  // );
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
 
   runApp(
     const ProviderScope(
